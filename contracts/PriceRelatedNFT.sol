87b6ab7aec731abb88551b9a971d0c06567a502c
Arthur <arakhiyarov@gmail.com>
2022-08-13 02:26:56 +0500

    second commit

--- contracts/PriceRelatedNFT.sol
+++ contracts/PriceRelatedNFT.sol
@@ -1,15 +1,21 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@pancakeswap/pancake-smart-contracts/projects/exchange-protocol/contracts/interfaces/IPancakeRouter01.sol';
 
 contract PriceRelatedNFT is ERC721 {
     string public constant TOKEN_URI =
-        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+        'ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json';
     uint256 private s_tokenCounter;
 
-    constructor() ERC721("Dogie", "DOG") {
+    constructor() ERC721('Dogie', 'DOG') {
         s_tokenCounter = 0;
+    }
+
+    modifier priceLevel() {
+        IPancakeRouter01 exchangeRate = IPancakeRouter01();
+        // Here we should get the exchange rate of our token to USDC on BSC
+        // exchangeRate.getAmountsOut();
     }
 }
