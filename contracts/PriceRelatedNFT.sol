aea08d19c8d970d9bcaf240cba00413bea3c9804
Arthur <arakhiyarov@gmail.com>
2022-08-14 20:44:20 +0500

    selfmade oracle

--- contracts/PriceRelatedNFT.sol
+++ contracts/PriceRelatedNFT.sol
@@ -1,30 +1,113 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '../interfaces/IPancakeRouter01.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract PriceRelatedNFT is ERC721 {
-    uint256 requiredPrice;
+contract PriceRelatedNFT is ERC721Enumerable, Ownable {
+    uint256 public priceLimit;
+    uint256 public s_tokenCounter;
+    uint256 public maxMintAmount;
+    uint256 public maxSupply;
+    uint256 public cost;
+    bool public paused = false;
     string public constant TOKEN_URI =
         'ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json';
-    uint256 private s_tokenCounter;
-    address private routerV2addr = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    address private ERC721NFTMarketV1addr =
+        0x17539cCa21C7933Df5c980172d22659B8C345C5A;
 
-    constructor(uint256 _requiredPrice) ERC721('Dogie', 'DOG') {
-        s_tokenCounter = 0;
-        requiredPrice = _requiredPrice;
+    // form ERC721NFTMarketV1 contract
+    struct Ask {
+        address seller; // address of the seller
+        uint256 price; // price of the token
     }
 
-    modifier priceLevel() {
-        IPancakeRouter01 exchangeRate = IPancakeRouter01(routerV2addr);
-        // Here we should get the exchange rate of our token to USDC on BSC
-        // exchangeRate.getAmountsOut();
-
-        ERC721NFTMarketV1 nftMarket = ERC721NFTMarketV1(
-            0x17539cCa21C7933Df5c980172d22659B8C345C5A
+    modifier priceLevel(uint256 tokenId) {
+        ERC721NFTMarketV1 nftMarket = ERC721NFTMarketV1(ERC721NFTMarketV1addr);
+        uint256[] memory tokenIds = new uint256[](1);
+        tokenIds[0] = tokenId;
+        askInfo = new Ask[](1);
+        (, askInfo) = nftMarket.viewAsksByCollectionAndTokenIds(
+            address(this),
+            tokenIds
         );
-        // (, Ask[] memory askInfo) = viewAsksByCollectionAndTokenIds(address collection, uint256[] calldata tokenIds)
-        require(askInfo[0].price > requiredPrice);
+        require(askInfo[0].price > priceLimit);
+    }
+
+    constructor(
+        uint256 _priceLimit,
+        uint256 _maxMintAmount,
+        uint256 _maxSupply,
+        uint256 _cost
+    ) ERC721('Dogie', 'DOG') {
+        s_tokenCounter = 0;
+        priceLimit = _priceLimit;
+        maxMintAmount = _maxMintAmount;
+        maxSupply = _maxSupply;
+        cost = _cost;
+    }
+
+    function mint(uint256 _mintAmount) public payable {
+        uint256 supply = totalSupply();
+        require(!paused);
+        require(_mintAmount > 0);
+        require(_mintAmount <= maxMintAmount);
+        require(supply + _mintAmount <= maxSupply);
+
+        if (msg.sender != owner()) {
+            require(msg.value >= cost * _mintAmount);
+        }
+
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+    }
+
+    function setCost(uint256 _newCost)
+        public
+        onlyOwner
+        returns (uint256 updatedCost)
+    {
+        cost = _newCost;
+        return cost;
+    }
+
+    function setmaxMintAmount(uint256 _newmaxMintAmount)
+        public
+        onlyOwner
+        returns (uint256 updatedMaxMintAmount)
+    {
+        maxMintAmount = _newmaxMintAmount;
+        return maxMintAmount;
+    }
+
+    // Pancakeswap nft market contract address
+    function setERC721NFTMarketV1addr(address newERC721NFTMarketV1addr)
+        public
+        onlyOwner
+        returns (address updatedERC721NFTMarketV1addr)
+    {
+        ERC721NFTMarketV1addr = newERC721NFTMarketV1addr;
+        return ERC721NFTMarketV1addr;
+    }
+
+    function setPriceLimit(uint256 newPriceLimit)
+        public
+        onlyOwner
+        returns (uint256 updatedPriceLimit)
+    {
+        priceLimit = newPriceLimit;
+        return priceLimit;
+    }
+
+    function pause(bool _state) public onlyOwner returns (bool updatedPaused) {
+        paused = _state;
+        return paused;
+    }
+
+    function withdraw() external payable onlyOwner {
+        uint256 bal = address(this).balance;
+        (bool os, ) = payable(owner()).call{ value: address(this).balance }('');
+        require(os);
     }
 }
